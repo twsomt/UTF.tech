eb60fc4da06c85d8f56c03b715274cf0b1f97afc
twsomt <volk0v-ak@yandex.ru>
2024-08-05 10:51:28 +0300

    PostgreSQL

--- utftech/utftech/settings.py
+++ utftech/utftech/settings.py
@@ -69,9 +69,13 @@ WSGI_APPLICATION = "utftech.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
